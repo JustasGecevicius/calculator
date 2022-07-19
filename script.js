33d6a3324474b2dc0299d492dae1e8e8721c0606
Zebaginas <j.gecevicius@lancaster.ac.uk>
2022-07-19 14:05:43 +0100

    completed the calculator with basic functions for a pair of numbers

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll("button");
-
+let input = document.querySelector("input");
+input.value = "222+100"
 
 function startUp() {
   let columns = 1;
@@ -11,30 +12,64 @@ function startUp() {
     }
     elem.style.gridColumn = `${columns}`;
     elem.style.gridRow = `${rows}`;
-    elem.addEventListener("click", (e) => {screen(e);})
+    elem.addEventListener("click", (e) => {
+      screen(e);
+    });
     columns++;
   });
 }
 startUp();
 
-function screen(event){
-
-    if(event.target.className == "=")
-    {
-        calc();
-        return;
-    }
-console.log(event.target.className)
-let input = document.querySelector("input");
-input.value = input.value + event.target.className;
-
+function screen(event) {
+  if (event.target.className == "=") {
+    calc();
+    return;
+  }
+  console.log(event.target.className);
+  let input = document.querySelector("input");
+  input.value = input.value + event.target.className;
 }
-function calc()
-{   
-    
-    console.log("zeba")
-    let input = document.querySelector("input");
-input.value = input.value = "";
+function calc() {
+
+  let repos = /^(\d+)[\/\*\-\+](\d+)$/;
+  let symbolRepos = /[\/\*\-\+]/;
+  let value = input.value;
+  let string = value.match(repos);
+  let symbol = string[0].match(symbolRepos);
+let a = Number(string[1]);
+let b = Number(string[2]);
+  switch (symbol[0]) 
+  {
+    case "+":
+      console.log(addition(a, b));
+      break;
+      case "-":
+      console.log(subtraction(a, b));
+      break;
+      case "*":
+      console.log(multiplication(a, b));
+      break;
+      case "/":
+      console.log(division(a, b));
+      break;
+  }
+
+
+  input.value = input.value = "";
 }
-
-
+function addition(a, b)
+{
+  return a + b;
+}
+function subtraction(a, b)
+{
+  return a - b;
+}
+function multiplication(a, b)
+{
+  return a * b;
+}
+function division(a, b)
+{
+  return a / b;
+}
