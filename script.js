7adb560d598e4389915c12b435417e76168d2012
Zebaginas <justas.gece@gmail.com>
2023-01-02 17:38:43 +0100

    changed the way of checking for incorrect inputs

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const mainRepos = /(\d+)|[\/\*\-\+]/g;
 let fullArray;
 let displayed = false;
 
-
 //a function to set up the number grid and add the event listeners to the buttons
 function startUp() {
   let columns = 1;
@@ -19,7 +18,9 @@ function startUp() {
     elem.addEventListener("click", (e) => {
       screen(e);
       elem.classList.add("buttonactive");
-      setTimeout(function elemremove (){elem.classList.remove("buttonactive")}, 100);
+      setTimeout(function elemremove() {
+        elem.classList.remove("buttonactive");
+      }, 100);
     });
     columns++;
   });
@@ -36,8 +37,7 @@ function screen(event) {
     return;
   }
   // checks if the answer has been displayed previously if yes, clears the screen and resets the displayed variable
-  if(displayed == true)
-  {
+  if (displayed == true) {
     inputField.value = "";
     displayed = false;
   }
@@ -48,99 +48,84 @@ function screen(event) {
 function calc() {
   //two loops to go over the whole array of numbers and arithmetic symbols
   //the first loop goes over the multiplication and division symbols;
-  for(let i = 0; i < fullArray.length; i++)
-  {
-    console.log(i);
-    if(fullArray[i] == "*" && i != 0)
-    {
-      let a = Number(fullArray[i - 1]);
-      let b = Number(fullArray[i + 1]);
-      multiplication(a, b, i);
-      i--;
-    }
-    else if(fullArray[i] == "/" && i != 0)
-    {
-      let a = Number(fullArray[i - 1]);
-      let b = Number(fullArray[i + 1]);
-      division(a, b, i);
-      i--;
+  for (let i = 1; i < fullArray.length; i++) {
+    if (/\d/.test(fullArray[i - 1]) && /\d/.test(fullArray[i + 1])) {
+      if (fullArray[i] == "*") {
+        let a = Number(fullArray[i - 1]);
+        let b = Number(fullArray[i + 1]);
+        multiplication(a, b, i);
+        i--;
+      } else if (fullArray[i] == "/") {
+        let a = Number(fullArray[i - 1]);
+        let b = Number(fullArray[i + 1]);
+        division(a, b, i);
+        i--;
+      }
+    } else {
+      fullArray = ["Error"];
+      break;
     }
   }
   //the second loop goes over the addition and subtraction symbols
-  for(let i = 0; i < fullArray.length; i++)
-  {
-    if(fullArray[i] == "+" && i != 0)
-    {
-      let a = Number(fullArray[i - 1]);
-      let b = Number(fullArray[i + 1]);
-      console.log("+");
-      addition(a, b, i);
-      i--;
-    }
-    else if(fullArray[i] == "-" && i != 0)
-    {
-      let a = Number(fullArray[i - 1]);
-      let b = Number(fullArray[i + 1]);
-      subtraction(a, b, i);
-      i--;
+  for (let i = 1; i < fullArray.length; i++) {
+    if (/\d/.test(fullArray[i - 1]) && /\d/.test(fullArray[i + 1])) {
+      if (fullArray[i] == "+") {
+        let a = Number(fullArray[i - 1]);
+        let b = Number(fullArray[i + 1]);
+        addition(a, b, i);
+        i--;
+      } else if (fullArray[i] == "-") {
+        let a = Number(fullArray[i - 1]);
+        let b = Number(fullArray[i + 1]);
+        subtraction(a, b, i);
+        i--;
+      }
+    } else {
+      fullArray = ["Error"];
+      break;
     }
   }
-  //displays the answer on the screen
-  if(typeof fullArray[0] != "number")
-  {
-    fullArray[0] = "Error";
-  }
-    inputField.value = fullArray[0];
-
-    displayed = true;
-
-
+  inputField.value = fullArray[0];
+  displayed = true;
 }
 
 //the mathematical functions
-function addition(a, b, i)
-{ 
-  if(typeof a != "number" || typeof b != "number")
-  {
-    inputField.value = "ERROR STUPID"
+function addition(a, b, i) {
+  if (typeof a != "number" || typeof b != "number") {
+    inputField.value = "ERROR";
     return;
   }
-  fullArray.splice(i - 1, 3, a + b);
-}
-function subtraction(a, b, i)
-{
-   
-  if(typeof a != "number" || typeof b != "number")
-  {
-    inputField.value = "ERROR STUPID"
-    return;
-  }
-  let x = a - b;  
+  let x = a + b;
   fullArray.splice(i - 1, 3, x);
 }
-function multiplication(a, b, i)
-{
-   
-  if(typeof a != "number" || typeof b != "number")
-  {
-    inputField.value = "ERROR STUPID";
+function subtraction(a, b, i) {
+  if (typeof a != "number" || typeof b != "number") {
+    inputField.value = "ERROR";
     return;
   }
-  let x = a * b;  
+  let x = a - b;
   fullArray.splice(i - 1, 3, x);
 }
-function division(a, b, i)
-{
-   
-  if(typeof a != "number" || typeof b != "number")
-  {
-    inputField.value = "ERROR STUPID";
+function multiplication(a, b, i) {
+  console.log(!/\d/.test(b), !/\d/.test(a));
+  if (!/\d/.test(a) || !/\d/.test(b)) {
+    inputField.value = "ERROR";
     return;
   }
-  let x = a / b;  
+  let x = a * b;
+  fullArray.splice(i - 1, 3, x);
+}
+function division(a, b, i) {
+  // console.log(typeof a, typeof b);
+  if (typeof a != "number" || typeof b != "number") {
+    inputField.value = "ERROR";
+    return;
+  }
+  //console.log("error");
+  let x = a / b;
   fullArray.splice(i - 1, 3, x);
 }
 
-let buttoneffect = function(e){
+let buttoneffect = function (e) {
   console.log(e);
-}
+};
